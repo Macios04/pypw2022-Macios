7cad536c1610a721d5fa47dc0c0ac532402d8920
Maciej Woźniewicz <maciej.wozniewicz01@gmail.com>
2022-10-09 14:55:29 +0200

    Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    even_nums = [
+        x for x in range(20) if x % 2 == 0
+    ]
+    print(even_nums)
+
+
+    values = [
+        (x, y)
+        for x in range(20)
+        for y in range(20)
+    ]
+    print(values)
+
+
+

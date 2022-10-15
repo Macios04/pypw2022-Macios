49d45e89862adc398585278e81e37b3925e1a0bb
Maciej Woźniewicz <maciej.wozniewicz01@gmail.com>
2022-10-15 10:36:56 +0200

    Commit

--- is_lucky.py
+++ is_lucky.py
@@ -10,4 +10,39 @@ if __name__ == "__main__":
     print(is_lucky("223214"))
     pass
 
-def is_lucky_gen(nums, n):
+
+def is_lucky_2(serial):
+    first_part = [int(x) for x in serial[:3]]
+    second_part = [int(x) for x in serial[3:]]
+    return sum(first_part) == sum(second_part)
+
+if __name__ == "__main__":
+    print(is_lucky("112004"))
+    pass
+
+def create_lucky_tickets(n):
+    result = []
+    i = 0
+    for i in range(10 ** 6):
+        serial = str(i).zfill(6)
+        if is_lucky(serial):
+            result.append(serial)
+        if len(result) >= n:
+            break
+    return result
+
+if __name__ == "__main__":
+    print(create_lucky_tickets(100))
+    pass
+
+def calculate_total_lucky_tickets():
+    total = 0
+    for i in range(10 ** 6):
+        serial = str(i).zfill(6)
+        if is_lucky(serial):
+            total += 1
+    return total
+
+if __name__ == "__main__":
+    print(calculate_total_lucky_tickets())
+    pass
--- my_max.py
+++ my_max.py
@@ -11,19 +11,40 @@ if __name__ == "__main__":
     pass
 
 def my_max3(a, b, c):
-    return max([a, b, c])
+    return my_max(my_max(a, b), c)
 
 if __name__ == "__main__":
     print(my_max3(1, 999, 222))
     pass
 
 def minmax(list):
-    result = (min(list), max(list))
-    return result
+    min_value = 0
+    max_value = 0
+    for item in list[1:]:
+        if item >= max_value:
+            max_value = item
+        if item < min_value:
+            min_value = item
+    return min_value, max_value
 
 if __name__ == "__main__":
     print(minmax([2, 3, 4, 5]))
     pass
+
+def sweap(items):
+    min_idx, max_idx = 0, 0
+    for i in range(len(items)):
+        if items[i] >= items[max_idx]:
+            max_idx = i
+        if items[i] < items[min_idx]:
+            min_idx = i
+    items[min_idx], items[max_idx] = items[max_idx], items[min_idx]
+    return items
+
+if __name__ == "__main__":
+    print(sweap([2, 3, 4, 5]))
+    pass
+
 def my_abs(x):
     if x >= 0:
         return x
@@ -34,7 +55,7 @@ if __name__ == "__main__":
     print(my_abs(-5))
     pass
 
-def filter_nums(list):       #nie działa
+def filter_nums(list):
     i = 0
     result = []
     for i in range(len(list)):
@@ -49,7 +70,7 @@ if __name__ == "__main__":
     print(filter_nums([3, 4, 5, 7, 6, 9, 8, 22, 33, 69]))
     pass
 
-def generate_evans(start, end):  #nie dziala
+def generate_evans(start, end):
     i = 0
     list1 = list(range(start, end))
     result = []
@@ -77,5 +98,9 @@ if __name__ == "__main__":
     print(compute([2, 3, 4, 12], 2, 3))
     pass
 
+def compute_2(nums, n, m):
+    return [x ** n % m for x in nums]
 
+if __name__ == "__main__":
+    print(compute_2([2, 3, 4, 12], 2, 3))
 

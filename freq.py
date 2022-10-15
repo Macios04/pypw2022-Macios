7ec4844a7efbb548babf414311d45a1594aa0873
Maciej Woźniewicz <maciej.wozniewicz01@gmail.com>
2022-10-15 14:30:40 +0200

    Commito00

--- freq.py
+++ freq.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 def calculate_freq(sentence):
     freq = {}
     for word in sentence.lower().split():
@@ -7,5 +8,13 @@ def calculate_freq(sentence):
             freq[word] = 1
     return freq
 
+def calculate_freq_2(sentence):
+    freq = defaultdict(int)
+    for word in sentence.lower().split():
+        freq[word] += 1
+    return freq
+
+
 if __name__ == "__main__":
     print(calculate_freq("This is a simple text"))
+    print(calculate_freq_2("This is a simple text text"))
